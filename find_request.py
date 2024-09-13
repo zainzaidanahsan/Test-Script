57c1c5308ec0932a41e1af67837e2ad2a7b7bd27
zainzaidanahsan <ahsanzidan60@gmail.com>
2024-09-13 15:36:55 +0700

    add new python file to find request

--- find_request.py
+++ find_request.py
@@ -18,19 +18,22 @@ tables = cursor.fetchall()
 # Mencari nilai di setiap tabel
 search_value = 'REQ0600642'
 for (table_name,) in tables:
-    cursor.execute(f"SHOW COLUMNS FROM {table_name}")
+    cursor.execute(f"SHOW COLUMNS FROM `{table_name}`")
     columns = cursor.fetchall()
     column_names = [column[0] for column in columns]
     
     # Mencari di setiap kolom
     for column_name in column_names:
-        query = f"SELECT * FROM {table_name} WHERE {column_name} = %s"
-        cursor.execute(query, (search_value,))
-        results = cursor.fetchall()
-        if results:
-            print(f"Found in table {table_name}, column {column_name}:")
-            for row in results:
-                print(row)
+        query = f"SELECT * FROM `{table_name}` WHERE `{column_name}` = %s"
+        try:
+            cursor.execute(query, (search_value,))
+            results = cursor.fetchall()
+            if results:
+                print(f"Found in table `{table_name}`, column `{column_name}`:")
+                for row in results:
+                    print(row)
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
 
 cursor.close()
 conn.close()

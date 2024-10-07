7daaa5b28918b7f15c359e1fbcbfce9471a031c5
zainzaidanahsan <ahsanzidan60@gmail.com>
2024-10-08 05:19:01 +0700

    Running

--- program.js
+++ program.js
@@ -80,7 +80,7 @@ class SnowArchival {
         let startIdx = 0;
         let totalProcessed = 0;
         let maxEntries = 5000;
-        let startTaskNumber = 'RITM0928838';
+        let startTaskNumber = 'RITM0919610';
 
         while (totalProcessed < maxEntries) {
             let tasks = await this.getTasks(startTaskNumber, this.batchSize);

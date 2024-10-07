feb60cb59c411092a8be4aa85dfddea0cc106594
zainzaidanahsan <ahsanzidan60@gmail.com>
2024-10-07 14:15:57 +0700

    add global gc

--- program.js
+++ program.js
@@ -80,7 +80,7 @@ class SnowArchival {
         let startIdx = 0;
         let totalProcessed = 0;
         let maxEntries = 5000;
-        let startTaskNumber = 'RITM1032760';
+        let startTaskNumber = 'RITM1023209';
 
         while (totalProcessed < maxEntries) {
             let tasks = await this.getTasks(startTaskNumber, this.batchSize);
